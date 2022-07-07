313d417e92f803b06989775f3736065cab6357c9
Tom Watson <tomwatson840@gmail.com>
2022-07-07 17:15:15 +0100

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send(true);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
