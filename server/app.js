dcd6a555c3ab3a0086b17f1a66c69ffa1bded4d0
watmot <tomwatson840@gmail.com>
2022-07-12 16:41:29 +0100

    Implement minor security updates

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const helmet = require('helmet');
 const passport = require('passport');
 const session = require('express-session');
 const Redis = require('ioredis');
@@ -14,16 +15,19 @@ const redisClient = new Redis();
 const sess = {
   store: new RedisStore({ client: redisClient }),
   secret: process.env.SESSION_SECRET,
+  name: 'sessionID',
   resave: true, // Setting resave and rolling means the cookie will be refreshed
   rolling: true, // on subequent requests, stopping the user's session ending during usage.
   saveUninitialized: false,
   cookie: {
+    httpOnly: true,
     maxAge: 60 * 60 * 1000, // Cookie expires after 1h, unless refreshed (see above).
     secure: process.env.NODE_ENV === 'production'
   }
 };
 
-// Middleware
+// Express Middleware
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,6 +41,7 @@ app.use(session(sess));
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(passport.initialize());
 app.use(passport.session());
+app.disable('x-powered-by');
 
 // Routes
 const auth = require('./routes/auth');
